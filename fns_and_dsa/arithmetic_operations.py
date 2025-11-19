61c84f40616a2e5d34a38fffd608df76a23dda3d
Stephen Akande <tosisiyesteve@gmail.com>
2025-11-19 03:04:38 +0100

    start with function task retry

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,11 @@
 def perform_operation(num1, num2, operation):
-  match operation:
-    case "add":
-      add(num1, num2)
-    case "subtract":
-      subtract(num1, num2)
-    case "multiply":
-      multiply(num1, num2)
-    case "divide":
-      divide(num1, num2)
-
-
   def add(num1, num2):
     """Returns the sum of 2 values num1 and num 2"""
     return num1 + num2
 
   def subtract(num1, num2):
     """Returns the difference of 2 values num1 and num 2"""
-    return num1 + num2
+    return num1 - num2
 
   def multiply(num1, num2):
     """Returns the product of 2 values num1 and num 2"""
@@ -28,4 +17,16 @@ def perform_operation(num1, num2, operation):
       return "Cannot divide by 0"
     else:
       return num1 / num2
+    
+  match operation:
+    case "add":
+      return add(float(num1), float(num2))
+    case "subtract":
+      return subtract(float(num1), float(num2))
+    case "multiply":
+      return multiply(float(num1), float(num2))
+    case "divide":
+      return divide(float(num1), float(num2))
+
+
 
