a34d1e1e08ef43a275036ace4bcfa49020a7cf76
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-04 22:19:04 +0100

    OOP task 1

--- oop/library_system.py
+++ oop/library_system.py
@@ -16,7 +16,7 @@ class EBook(Book):
     self.file_size = file_size
 
   def __str__(self):
-    return f"{super().__str__()} File Size: {self.file_size}KB"
+    return f"{super().__str__()}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -26,7 +26,7 @@ class PrintBook(Book):
     self.page_count = page_count
 
   def __str__(self):
-    return f"{super().__str__()} Page Count: {self.page_count}"
+    return f"{super().__str__()}, Page Count: {self.page_count}"
 
 
 class Library:
